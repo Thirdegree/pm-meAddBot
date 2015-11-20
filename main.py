bb86d9e6b7dc6c042e6b3a8a56e7051adf0cb5fa
Joel Christiansen <greenbadge.jc@gmail.com>
2015-11-19 20:18:22 -0700

    contributor, not mod. 'one and done' what a dummy

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ regString = r"(?i)pm[_-]me.*"
 
 r = obot.login()
 sub = r.get_subreddit("pm-me")
-current_mods = [i for i in sub.get_moderators()]
+current_mods = [i for i in sub.get_contributors()]
 
 def main():
     comment_stream = praw.helpers.comment_stream(r, 'all')
     for comment in comment_stream:
         if re.match(regString, comment.author.name) and comment.author not in current_mods:
-            sub.add_moderator(comment.author)
+            sub.add_contributor(comment.author)
             current_mods.append(comment.author)
 
 

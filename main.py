8066a38b4a1c59f041d918d4520fa17458599ea3
Joel Christiansen <greenbadge.jc@gmail.com>
2015-11-19 20:13:09 -0700

    don't invite people who are already mods

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ regString = r"(?i)pm[_-]me.*"
 
 r = obot.login()
 sub = r.get_subreddit("pm-me")
+current_mods = [i for i in sub.get_moderators()]
 
 def main():
     comment_stream = praw.helpers.comment_stream(r, 'all')
     for comment in comment_stream:
-        if re.match(regString, comment.author.name):
+        if re.match(regString, comment.author.name) and comment.author not in current_mods:
             sub.add_moderator(comment.author)
+            current_mods.append(comment.author)
 
 
 if __name__ == '__main__':
